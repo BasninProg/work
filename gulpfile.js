92d9e78d45a6ce0c854c197ce3e444245a1912f2
basnin <basnin@example.ru>
2015-10-29 17:02:47 +0200

    версия получше(1.0)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require("gulp"),
 	connect = require("gulp-connect"),
-	opn = require("opn");
+	opn = require("opn"),
+  concatCSS = require("gulp-concat-css"),
+  rename = require("gulp-rename"),
+  minifyCSS = require("gulp-minify-css"),
+  autoprefixer = require('gulp-autoprefixer');
 
 // Запускаем локальный сервер
 gulp.task('connect', function() {
@@ -18,24 +22,32 @@ gulp.task('html', function () {
     .pipe(connect.reload());
 });
 
-//Работа с CSS
-gulp.task('css', function () {
-  gulp.src('./app/css/*.css')
-    .pipe(connect.reload());
-});
-
 //Работа с JS
 gulp.task('js', function () {
   gulp.src('./app/js/*.js')
     .pipe(connect.reload());
 });
 
+//Concat css, minify, rename, autoprefixer
+gulp.task('concatCSS', function () {
+  gulp.src('./app/css/**/*.css')
+    .pipe(concatCSS('main-style.css'))
+    .pipe(autoprefixer({
+            browsers: ['last 15 versions'],
+            cascade: false
+        }))
+    .pipe(minifyCSS(''))
+    .pipe(rename('main-style.min.css'))
+    .pipe(gulp.dest('./app/css'))
+    .pipe(connect.reload());
+});
+
 //Слежка
 gulp.task('watch', function () {
   gulp.watch(['./app/*.html'], ['html']);
-  gulp.watch(['./app/css/*.css'], ['css']);
+  gulp.watch(['./app/css/*.css'], ['concatCSS']);
   gulp.watch(['./app/js/*.js'], ['js']);
 });
 
 //Задача по умолчанию
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['connect', 'concatCSS', 'watch']);
